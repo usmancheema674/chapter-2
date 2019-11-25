05b1f2bb0eccebc559f5147fb5a4507cd5ac9af3
usmancheema674 <58159427+usmancheema674@users.noreply.github.com>
2019-11-25 08:20:45 -0800

    Add files via upload

---
+++ chap 2 example 2.26.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cin>>a;
+cout<<"\n";
+cin>>b;
+c=a%b;
+if(c==0){
+	cout<<"modulus is not found ";
+}	
+else
+{
+	cout<<"modulus is found";}
+	}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
